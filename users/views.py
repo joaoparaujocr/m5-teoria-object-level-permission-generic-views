9146f0f998fda97591ce93683805b61116417d32
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2022-12-22 19:34:58 -0400

    code example

--- users/views.py
+++ users/views.py
@@ -5,12 +5,13 @@ from rest_framework.permissions import IsAuthenticated
 from users.serializers import ComputerSerializer
 
 from .models import Computer
+from .permissions import isComputerOwner
 
 
 # Create your views here.
 class RetrieveComputerView(generics.RetrieveAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, isComputerOwner]
 
     queryset = Computer.objects.all()
     serializer_class = ComputerSerializer
